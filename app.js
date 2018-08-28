c16bff300d4a6c8a0f6f62a787989e44ad770b4b
Xavier Duncan <xduncan@pm.me>
2018-08-28 15:34:52 -0400

    set access token as global variable

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const passport = require('passport');
 const passportSetup = require('./config/passport-setup');
 const authRoutes = require('./routes/auth-routes');
 
 const app = express();
+
 // setup view engine
 app.set('view engine', 'ejs');
+
+// passport initialize + session
+app.use(passport.initialize());
+app.use(passport.session());
+
+// passport serialize user
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+// passport deserialize user
+passport.deserializeUser(function(user, done) {
+    done(null, user);
+});
 
 // setup routes
 app.use('/auth', authRoutes);
@@ -14,6 +29,7 @@ app.get('/', (req, res) => {
     res.render('index')
 });
 
+// app runs on port 3000
 app.listen(3000, () =>
     console.log('buddy-up is running on port 3000!')
 );
