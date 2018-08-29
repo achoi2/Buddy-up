8144ef89b94b4fa029b09c16aad0c631933902ea
Xavier Duncan <xduncan@pm.me>
2018-08-29 12:06:39 -0400

    add fetch request to Meetup API

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const passport = require('passport');
 const passportSetup = require('./config/passport-setup');
 const authRoutes = require('./routes/auth-routes');
+const isoFetch = require('isomorphic-fetch');
 
 const app = express();
 
@@ -11,7 +12,6 @@ app.set('view engine', 'ejs');
 // passport initialize + session
 app.use(passport.initialize());
 app.use(passport.session());
-
 // passport serialize user
 passport.serializeUser(function(user, done) {
     done(null, user);
@@ -21,12 +21,26 @@ passport.deserializeUser(function(user, done) {
     done(null, user);
 });
 
-// setup routes
+// setup authentication routes
 app.use('/auth', authRoutes);
 
 // create home route
 app.get('/', (req, res) => {
     res.render('index')
+});
+
+// fetch user profile from Meetup API
+fetch(
+    'https://api.meetup.com/members/self', 
+    {headers: new Headers(
+        {"Authorization": "Bearer b81eb53e6febf5a1cdf3487314faffae"}
+    )}
+)
+.then(res => {
+    return res.json();
+})
+.then(data => {
+    console.log(JSON.stringify(data));
 });
 
 // app runs on port 3000
