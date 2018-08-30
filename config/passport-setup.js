a3abb5be42f464f50578ee97719318c6306865c9
Liz Khuu <khuu.liz@gmail.com>
2018-08-30 15:35:54 -0400

    Added functions to get Meetup ID and store it with the Access Token in the database

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,18 +1,44 @@
 require('dotenv').config();
 const passport = require('passport');
 const MeetupStrategy = require('passport-meetup-oauth2').Strategy;
+const db = require('./database');
+let serverURL = 'http://localhost:3000/auth/meetup/redirect';
+
+let getMeetupProfile = (token) => {
+    let url = 'https://api.meetup.com/members/self'
+    return fetch(url,
+        {headers: new Headers(
+            {"Authorization": "Bearer " + token}
+        )}
+    )
+    .then(res => {
+        return res.json();
+    })
+};
+
+let storeMeetupProfile = (id, accessToken) => {
+    db.query(`UPDATE users SET access_token = $1
+    WHERE meetup_id = '$2';`, [accessToken, id]
+    )}
 
 var registerMeetupStrategy = () => {
     passport.use(
         new MeetupStrategy({
             // options for the meetup strategy
-            callbackURL: 'http://ec2-18-191-243-243.us-east-2.compute.amazonaws.com/auth/meetup/redirect',
+            callbackURL: serverURL,
             clientID: process.env.CLIENT_ID,
             clientSecret: process.env.CLIENT_SECRET
         }, (accessToken, refreshToken, profile, done) => {
             // passport callback function
-            console.log(accessToken);
-            return done(null, profile);
+            console.log(`Access Token: ${accessToken}`);
+            getMeetupProfile(accessToken)
+            .then(profile => {
+                console.log(profile);
+                return storeMeetupProfile(profile.id, accessToken)
+            })
+            .then(data => {
+                return done(null, profile);
+            });
         })
     );
 }
